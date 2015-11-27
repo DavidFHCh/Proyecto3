14e0a6ec80baceac4dd90594770dad830bab3ce8
Gilberto López <gilisaac@ciencias.unam.mx>
2015-11-27 10:23:55 -0600

    Esqueleto de la aplicación.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Librosciencias::Application.config.secret_token = 'e030981c1ea8b982c8ea23eaa15a29b1bc0dde047a81134f0a27ec6f50f37690e3ebb511151b73f3e8df1cc10c7940d494f83ce555ed516b1f07b7bec654f4fb'
