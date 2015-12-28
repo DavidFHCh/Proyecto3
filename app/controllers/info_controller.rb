54e8b942b84176cb0e465ebcd82e9b323ce3ad9a
Gilberto-Lopez <gilisaac@ciencias.unam.mx>
2015-12-27 23:56:46 -0600

    La carrera se solicita al usuario por medio de un select que lista las carreras de Ciencias.

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -3,5 +3,6 @@ class InfoController < ApplicationController
   def ask
     @title = @header = "Información personal"
     @max_year = Time.new.year + 1
+    @carreras = Carrera.all.map { |carrera| [carrera.nombre, carrera.clave] }
   end
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,30 +1,31 @@
 class Usuario < ActiveRecord::Base
   belongs_to :facultade, class_name: "Facultade", foreign_key: "facultad"
-  belongs_to :carrera, class_name: "Carrera", foreign_key: "carrera"
+  belongs_to :_carrera, class_name: "Carrera", foreign_key: "carrera"
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.nombre = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.correo = auth.info.email
-      user.facultad = 3
-      user.tipo = 0
-      user.generacion = 0
-      user.carrera = Carrera.find(104)
-      user.password = ""
-      user.save!
+    usuario = where(provider: auth.provider, uid: auth.uid).first
+    unless usuario
+      usuario = Usuario.new
+      usuario.provider = auth.provider
+      usuario.uid = auth.uid
+      usuario.nombre = auth.info.name
+      usuario.oauth_token = auth.credentials.token
+      usuario.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      usuario.correo = auth.info.email
+      usuario.facultad = 3
+      usuario.tipo = 0
+      usuario.generacion = 0
+      usuario.carrera = 3
+      usuario.password = ""
+      usuario.save!
     end
   end
-
+  
   def semestre
-    return 0 if carrera.nil?
     time = Time.new
     s = (time.year - (generacion - 1)) * 2 + 1
     s += 1 unless time.month > 6
-    return 0 unless carrera.semestres < s
+    0 unless _carrera.semestres < s
     s
   end
 end
